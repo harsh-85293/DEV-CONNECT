0901d0eea34a95c4a735ee8587093a7dc3bd02d5
harsh-85293 <harsh.ramchandani122003@gmail.com>
2025-08-27 13:40:45 +0530

    database changed

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,14 @@
 const mongoose  = require("mongoose");
 
-const connectdb = async() => {
+const connectdb = async () => {
+  try {
     await mongoose.connect(
-        "mongodb+srv://harsh:ramchandani@final.lnxlk6l.mongodb.net/"
-    )
-}
+      "mongodb+srv://namastedev:LWmNZnauCZmpLng7@namastenode.oor8bmj.mongodb.net/devconnect"
+    );
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error(" Database connection failed:", err.message);
+  }
+};
 
 module.exports = connectdb;
-
